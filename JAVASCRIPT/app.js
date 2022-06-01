8305e0866e8a8736f2e6af0cf913be8a110eea11
Kevin Le <kevin@fullcycle.io>
2022-06-01 17:31:57 -0500

    cleaned up code, css implentations, albumn details, added new funtionality with button

--- JAVASCRIPT/app.js
+++ JAVASCRIPT/app.js
@@ -4,7 +4,12 @@ let spinner = document.getElementById("spinner");
 let splitScreen = document.querySelector(".search");
 let resultCount = document.querySelector(".result-count");
 let addTenResults = document.querySelector(".addTen");
-let limit = 200;
+let albumnDetails = document.querySelector('.albumn-details')
+addTenResults.classList.add('hidden')
+let limit = 10;
+let searchTerm = "";
+let results = [];
+
 
 function handleFormSubmit(event) {
   event.preventDefault();
@@ -12,34 +17,48 @@ function handleFormSubmit(event) {
   getData();
 }
 
+function displayResults() {
+  const count = Math.min(limit, results.length);
+  showResults.textContent = "";
+  resultCount.innerHTML = `
+    <p>Search Result: ${count} for ${searchTerm}</p>
+    `;
+  for (var i = 0; i < count; i++) {
+    let img = results[i].artworkUrl100;
+    let songTitle = results[i].collectionName;
+    let name = results[i].artistName;
+
+    let render = `
+        <div class = "album-results">
+        <img src="${img}" alt="">
+        <h3> ${name} </h3>
+        <h2> ${songTitle} </h2>
+        </div>
+        `;
+    showResults.insertAdjacentHTML("beforeEnd", render);
+    showResults.lastElementChild.addEventListener('click', function(){
+        albumnDetails.innerHTML = `
+        <img src="${img}" alt="">
+        <p>${name}</p>
+        <p>${songTitle}</p>
+        `
+    }) 
+  }
+}
+
 function getData() {
-  let searchTerm = document.querySelector("#user-input").value;
+  results = [];
+  limit = 10;
+  searchTerm = document.querySelector("#user-input").value;
   fetch(
-    `https://itunes.apple.com/search?term=${searchTerm}&media=music&entity=album&attribute=artistTerm&limit=${limit}`
+    `https://itunes.apple.com/search?term=${searchTerm}&media=music&entity=album&attribute=artistTerm&limit=200`
   )
     .then(function (data) {
       return data.json();
     })
     .then(function (json) {
-      let count = json.resultCount;
-      showResults.textContent = "";
-      resultCount.innerHTML = `
-        <p>Search Result: ${count} for ${searchTerm}</p>
-        `;
-      for (var i = 0; i < json.results.length; i++) {
-        let img = json.results[i].artworkUrl100;
-        let songTitle = json.results[i].collectionName;
-        let name = json.results[i].artistName;
-
-        let render = `
-            <div class = "album-results">
-            <img src="${img}" alt="">
-            <h3> ${name} </h3>
-            <h2> ${songTitle} </h2>
-            </div>
-            `;
-        showResults.insertAdjacentHTML("beforeEnd", render);
-      }
+      results = json.results;
+      displayResults();
     })
     .catch(function (e) {
       console.error(e);
@@ -48,6 +67,7 @@ function getData() {
 
 function flexScreen() {
   spinner.classList.add("showSpinner");
+  addTenResults.classList.remove('hidden')
   setTimeout(() => {
     spinner.classList.remove("showSpinner");
   }, 1000);
@@ -55,12 +75,8 @@ function flexScreen() {
 }
 
 function handleAdd10Results() {
-  if (limit = 200) {
-    limit += 10;
-    getData();
-  } else {
-      return
-  }
+  limit += 10;
+  displayResults();
 }
 
 form.addEventListener("submit", handleFormSubmit);

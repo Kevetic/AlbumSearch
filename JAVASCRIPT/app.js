a97b007c91ef403212fd2cc4a2b2257233cf4630
Kevin Le <kevin@fullcycle.io>
2022-06-01 17:02:48 -0500

    solution 1, but api doesnt allow it

--- JAVASCRIPT/app.js
+++ JAVASCRIPT/app.js
@@ -1,70 +1,67 @@
-let showResults = document.querySelector(".results")
-let form = document.querySelector(".search-input")
-let input = document.querySelector(".user-input")
-let button = document.querySelector(".search-btn")
+let showResults = document.querySelector(".results");
+let form = document.querySelector(".search-input");
 let spinner = document.getElementById("spinner");
-let splitScreen = document.querySelector(".search")
-let resultCount = document.querySelector('.result-count')
-let addTenResults = document.querySelector('.addTen')
+let splitScreen = document.querySelector(".search");
+let resultCount = document.querySelector(".result-count");
+let addTenResults = document.querySelector(".addTen");
+let limit = 200;
 
-// window.onload = emptyInput()
-form.addEventListener("submit", function(event) {
-    flexScreen()
-    // console.log(input.value)
-    event.preventDefault()
-
-})
+function handleFormSubmit(event) {
+  event.preventDefault();
+  flexScreen();
+  getData();
+}
 
 function getData() {
-    let searchTerm = document.querySelector("#user-input").value
-    fetch(`https://itunes.apple.com/search?term=${searchTerm}&media=music&entity=album&attribute=artistTerm&limit=200`)
-    .then (function (data){
-        return data.json()
+  let searchTerm = document.querySelector("#user-input").value;
+  fetch(
+    `https://itunes.apple.com/search?term=${searchTerm}&media=music&entity=album&attribute=artistTerm&limit=${limit}`
+  )
+    .then(function (data) {
+      return data.json();
     })
-    .then (function(json) {
-        let count = json.resultCount
-        let newArray = json.results
-        console.log(newArray)
-        showResults.textContent = ""
-        resultCount.innerHTML = `
-
+    .then(function (json) {
+      let count = json.resultCount;
+      showResults.textContent = "";
+      resultCount.innerHTML = `
         <p>Search Result: ${count} for ${searchTerm}</p>
-        `
-        for (var i = 0; i < json.results.length; i++) {
-            let img = json.results[i].artworkUrl100
-            let songTitle = json.results[i].collectionName
-            let name = json.results[i].artistName
-            
-            let render = `
+        `;
+      for (var i = 0; i < json.results.length; i++) {
+        let img = json.results[i].artworkUrl100;
+        let songTitle = json.results[i].collectionName;
+        let name = json.results[i].artistName;
+
+        let render = `
             <div class = "album-results">
             <img src="${img}" alt="">
             <h3> ${name} </h3>
             <h2> ${songTitle} </h2>
             </div>
-            `
-            showResults.insertAdjacentHTML("beforeEnd", render)
-            spliceFunc(newArray)
-        }
+            `;
+        showResults.insertAdjacentHTML("beforeEnd", render);
+      }
     })
+    .catch(function (e) {
+      console.error(e);
+    });
 }
-function flexScreen(){
-    spinner.classList.add('showSpinner');
-    setTimeout(() => {
-    console.log('spinner test')
-    spinner.classList.remove('showSpinner');
-    getData()
+
+function flexScreen() {
+  spinner.classList.add("showSpinner");
+  setTimeout(() => {
+    spinner.classList.remove("showSpinner");
   }, 1000);
-    splitScreen.classList.remove('split')
-
+  splitScreen.classList.remove("split");
 }
 
-// limit the search to 20
-// button + 10
-
-function spliceFunc(data) {
-    data.slice(0, 5)
-
-    addTenResults.addEventListener('click', function(){
-        console.log('add ten')
-    })
+function handleAdd10Results() {
+  if (limit = 200) {
+    limit += 10;
+    getData();
+  } else {
+      return
+  }
 }
+
+form.addEventListener("submit", handleFormSubmit);
+addTenResults.addEventListener("click", handleAdd10Results);
